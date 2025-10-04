baf25e60697c44afc420b172489c5141d2f57839
Kirubel Mesfin <164068364+kirubelm1@users.noreply.github.com>
2025-10-04 23:53:36 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Root element not found. Ensure there is a <div id="root"> in your HTML.');
 }
+
 
 createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
